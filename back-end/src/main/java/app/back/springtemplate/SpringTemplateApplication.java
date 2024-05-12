5930fa064deb11490b8f9270bd86ea667c4457e4
antonioclp <antonionetosg6@gmail.com>
2024-05-12 12:22:44 -0300

    first branch-back-end commit

--- back-end/src/main/java/app/back/springtemplate/SpringTemplateApplication.java
+++ back-end/src/main/java/app/back/springtemplate/SpringTemplateApplication.java
@@ -3,6 +3,9 @@ package app.back.springtemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Back-end branch.
+ */
 @SpringBootApplication
 public class SpringTemplateApplication {
 
