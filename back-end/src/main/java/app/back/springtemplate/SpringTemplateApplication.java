5f457935350b3d84a3995af1f2414d006847d3b8
antonioclp <antonionetosg6@gmail.com>
2024-05-13 09:57:45 -0300

    feat: final implementations

--- back-end/src/main/java/app/back/springtemplate/SpringTemplateApplication.java
+++ back-end/src/main/java/app/back/springtemplate/SpringTemplateApplication.java
@@ -3,9 +3,6 @@ package app.back.springtemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Back-end branch.
- */
 @SpringBootApplication
 public class SpringTemplateApplication {
 
