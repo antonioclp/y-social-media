4f07e32d2a5035c84c5b87dde5a387e89ce58b63
antonioclp <antonionetosg6@gmail.com>
2024-06-13 12:39:19 -0300

    feat: get comments and render messages w sorted by date

--- front-end/src/app/post/[id]/page.tsx
+++ front-end/src/app/post/[id]/page.tsx
@@ -1,36 +1,66 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { redirect, usePathname } from "next/navigation";
-import { useEffect } from "react";
 import Cookies from "js-cookie";
+
+// Interfaces
+import { IGenericResponse, IGetComments } from "@/app/utils/interfaces";
 
 // Api
 import { genericFetch } from "@/app/utils/api";
 
 export default function Post() {
+  const [postComments, setPostComments] = useState<IGetComments[]>([]);
   const id = usePathname().split("/").pop();
 
   useEffect(() => {
-    if (!Cookies.get("dXNy")) {
-      redirect("/login");
+    try {
+      if (!Cookies.get("dXNy")) {
+        redirect("/login");
+      }
+
+      const fetch = async () => {
+        const obj: IGenericResponse = await genericFetch({
+          option: "read-comments-by-post",
+          method: "GET",
+          endpoint: `comments/post/${id}`,
+        });
+
+        const { data }: IGenericResponse = obj;
+
+        if (Array.isArray(data)) {
+          const comments: IGetComments[] = data;
+
+          const sortedComments = comments.sort((cA, cB) => {
+            const dateComparison = cB.createdDate.localeCompare(cA.createdDate);
+
+            if (dateComparison === 0) {
+              return cB.createdTime.localeCompare(cA.createdTime);
+            }
+
+            return dateComparison;
+          });
+
+          setPostComments(sortedComments);
+        }
+      };
+
+      fetch();
+    } catch (err) {
+      console.error(err);
     }
-
-    const fetch = async () => {
-      const data = await genericFetch({
-        option: "read-comments-by-post",
-        method: "GET",
-        endpoint: `comments/post/${id}`,
-      });
-
-      console.log(data);
-    };
-
-    fetch();
   }, [id]);
 
   return (
     <main>
-      <section>Post by id</section>
+      <section>
+        {postComments.length > 0
+          ? postComments.map((c) => {
+              return <section key={c.commentId}>{c.message}</section>;
+            })
+          : null}
+      </section>
     </main>
   );
 }
